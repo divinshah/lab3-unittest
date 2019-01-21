f8f8d81d141df71f20b7b653746b23296dddefb7
anjalipanchal1124 <anjalipanchal1124@gmail.com>
2019-01-21 17:05:30 -0500

    new change

--- Lab-3-unittest-test.js
+++ Lab-3-unittest-test.js
@@ -1,4 +1,18 @@
 //LAB 3 - UNIT TESTING
 //======== THIS FILE IS FOR THE UNIT TEST ==========
-alert("lab 3 UNIT TEST");//please delete this line once connected.
+//alert("lab 3 UNIT TEST");//please delete this line once connected.
+
+function test_checkhumbrID(valueIn, expected){
+    "use strict";
+    let result = checkhumbrID(valueIn) 
+    let msg = "Value : " + valueIn + "| Result :" + result + "| Expected :" + expected + "<br/>";
+    
+    let msgBox = document.getElementById("data");
+    msgBox.innerHTML += msg;
+    
+    
+}
+test_checkhumbrID("a12345678",false);
+
+
 
